9240915e2531b21cf9b082c98f073f3a4300fd17
Shreyas <sunku.shreyas8@gmail.com>
2021-11-17 12:55:56 +0530

    update

--- Project_Movie/settings.py
+++ Project_Movie/settings.py
@@ -137,6 +137,8 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
 ]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.StaticFilesStorage'
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
