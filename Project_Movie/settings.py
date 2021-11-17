53a58619d49b28c723c92f5f045dc7026258fa50
Shreyas <sunku.shreyas8@gmail.com>
2021-11-17 12:28:11 +0530

    updated

--- Project_Movie/settings.py
+++ Project_Movie/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-ljb-_(64@)$wnc5c-0hzdxw1kz@+fk8y!lqe0^!dr*p#nnsl%@
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 # Application definition
 
