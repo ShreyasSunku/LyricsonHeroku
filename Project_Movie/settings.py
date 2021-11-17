f0d5aa4dcf67dfb6773847f6d4f54aa8c85f8d8b
Shreyas <sunku.shreyas8@gmail.com>
2021-11-17 15:08:14 +0530

    data

--- Project_Movie/settings.py
+++ Project_Movie/settings.py
@@ -87,6 +87,10 @@ DATABASES = {
     }
 }
 
+import dj_database_url
+db_from_env = dj_database_url.config(conn_max_age=600)
+DATABASES['default'].update(db_from_env)
+
 # DATABASES = {
 #     'default': {
 #         'ENGINE': 'sql_server.pyodbc',
